0d6fa9b033be4406597817c02d53c85b4b79e3b7
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-02-23 12:24:16 +0530

    Update 787.kt

--- 501-1000/787.kt
+++ 501-1000/787.kt
@@ -1,46 +1,11 @@
-fun findCheapestPrice(n: Int, flights: Array<IntArray>, src: Int, dst: Int, K: Int): Int {
-    val map = HashMap<Int, ArrayList<IntArray>>()
 
-    fun buildGraph() {
-        for (f in flights) {
-            map.getOrPut(f[0], { ArrayList() }).add(intArrayOf(f[1], f[2]))
-        }
+    fun findCheapestPrice(n: Int, flights: Array<IntArray>, src: Int, dst: Int, k: Int): Int {
+      val costs = IntArray(n) { Int.MAX_VALUE / 2 }
+      costs[src] = 0
+      repeat(k + 1) {
+        val prev = costs.clone()
+        for ((f, t, c) in flights) 
+            costs[t] = min(costs[t], prev[f] + c)
+      }
+      return costs[dst].takeIf { it < Int.MAX_VALUE / 2 } ?: -1
     }
-
-    fun bfs(): Int {
-        var stops = 0
-        var min = Int.MAX_VALUE
-        val q = ArrayDeque<IntArray>()
-        
-        q.offer(intArrayOf(src, 0))
-        
-        while (!q.isEmpty()) {
-            val size = q.size
-            
-            for (dummy in 0..size - 1) {
-                val node = q.poll()
-                val city = node[0]
-                val cost = node[1]
-
-                if (city == dst) min = minOf(min, cost)
-
-                if (map.contains(city)) {
-                    for (nei in map[city]!!) {
-                        // cut branch
-                        val newCost = cost + nei[1]
-                        if (newCost > min) continue
-                        q.offer(intArrayOf(nei[0], newCost))
-                    }
-                }
-            }
-            
-            if (stops++ > K) break
-        }
-        
-        return if (min == Int.MAX_VALUE) -1 else min
-    }
-
-    buildGraph()
-
-    return bfs()
-}
