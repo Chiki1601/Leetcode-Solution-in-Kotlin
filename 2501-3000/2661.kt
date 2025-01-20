887804fb5eb15614e5fea60be3b6bb9364971502
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-01-20 10:07:00 +0530

    Update 2661.kt

--- 2501-3000/2661.kt
+++ 2501-3000/2661.kt
@@ -1,21 +1,25 @@
-fun firstCompleteIndex(arr: IntArray, mat: Array<IntArray>): Int {
-    val numRows: Int = mat.size
-    val numCols: Int = mat.first().size
-    
-    val matrixLocations = HashMap<Int, Pair<Int, Int>>(numRows * numCols)
-    for (row: Int in 0 until numRows) {
-        for (col: Int in 0 until numCols) {
-            matrixLocations[mat[row][col]] = Pair(row, col)
-        }
-    }
-    
-    val rowPaintedCellCounts = IntArray(size = numRows)
-    val colPaintedCellCounts = IntArray(size = numCols)
+// 9ms
+class Solution {
+    fun firstCompleteIndex(arr: IntArray, mat: Array<IntArray>): Int {
+        val m = mat.size
+        val n = mat[0].size
 
-    return arr
-        .indices
-        .first {
-            val (row: Int, col: Int) = matrixLocations.getValue(arr[it])
-            ++rowPaintedCellCounts[row] == numCols || ++colPaintedCellCounts[col] == numRows
+        val table = Array(m * n + 1) { IntArray(2) }
+
+        val rows = IntArray(m)
+        val cols = IntArray(n)
+
+        for (i in 0 ..< m) {
+            for (j in 0 ..< n) {
+                table[mat[i][j]] = intArrayOf(i, j)
+            }
         }
+
+        var i = 0
+        for (a in arr) {
+            if (++rows[table[a][0]] == n || ++cols[table[a][1]] == m) return i
+            i++
+        }
+        return i
+    }
 }
