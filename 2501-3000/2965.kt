164e55c7dfc2c41bd270dac6bd57dcbbee21359e
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-03-06 10:46:53 +0530

    Update 2965.kt

--- 2501-3000/2965.kt
+++ 2501-3000/2965.kt
@@ -1,12 +1,27 @@
-fun findMissingAndRepeatedValues(grid: Array<IntArray>): IntArray {
-    val flat = grid.flatMap(IntArray::toList)
-    val range = 1..grid.count() * grid.count()
-    val diff = (range - flat.toSet()).firstOrNull() ?: -1
-    val twice = flat
-        .groupingBy { it }
-        .eachCount()
-        .entries
-        .firstOrNull { it.value > 1 }
-        ?.key ?: -1
-    return intArrayOf(twice, diff)
+class Solution {
+    fun findMissingAndRepeatedValues(grid: Array<IntArray>): IntArray {
+        val n = grid.size
+
+        val map = mutableMapOf<Int,Int>()
+
+        for (i in 0 until n){
+            for (j in 0 until n){
+                val num = grid[i][j]
+                map[num] = map.getOrDefault(num, 0) + 1
+            }
+        }
+        val ans = IntArray(2)
+        for ((key, value) in map){
+            if (value == 2){
+                ans[0] = key
+            }
+        }
+
+        for (i in 1 until n * n + 1){
+            if (i !in map){
+                ans[1] = i
+            }
+        }
+        return ans
+    }
 }
