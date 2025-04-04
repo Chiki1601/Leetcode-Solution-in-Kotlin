32096181f30bb91326be9aa6037d1007f5af1f8c
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-04-04 10:18:50 +0530

    Update 1123.kt

--- 1001-1500/1123.kt
+++ 1001-1500/1123.kt
@@ -1,53 +1,54 @@
-fun lcaDeepestLeaves(root: TreeNode?): TreeNode? {
-    fun getDepth(root: TreeNode?): Int {
-        if (root == null) return 0
-
-        return maxOf(getDepth(root.left), getDepth(root.right)) + 1
-    }
-    
-    val deepest = getDepth(root)
-    
-    fun getLCA(root: TreeNode?, depth: Int): TreeNode? {
-        // base case
-        if (root == null) return null
-
-        if (depth == deepest)  return root
-
-        // divide
-        val left = getLCA(root.left, depth + 1)
-        val right = getLCA(root.right, depth + 1)
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun lcaDeepestLeaves(root: TreeNode?): TreeNode? {
+        val parents = mutableMapOf<TreeNode, TreeNode?>()
+        val depths = mutableMapOf<TreeNode, Int>()
         
-        // conquer
-        return when {
-            left != null && right != null -> root
-            left != null -> left
-            else -> right
+        fun dfs(node: TreeNode?, parent: TreeNode?, depth: Int) {
+            if (node == null) {
+                return
+            }
+            
+            parents[node] = parent
+            depths[node] = depth
+            dfs(node.left, node, depth + 1)
+            dfs(node.right, node, depth + 1)
         }
+        
+        dfs(root, null, 0)
+        
+        var maxDepth = 0
+        for ((_, depth) in depths) {
+            maxDepth = max(maxDepth, depth)
+        }
+        
+        val maxDepthNodes = mutableSetOf<TreeNode>()
+        for ((node, depth) in depths) {
+            if (depth == maxDepth) {
+                maxDepthNodes.add(node)
+            }
+        }
+        
+        while (maxDepthNodes.size > 1) {
+            val newNodes = mutableSetOf<TreeNode>()
+            for (node in maxDepthNodes) {
+                parents[node]?.let { 
+                    newNodes.add(it) 
+                }
+            }
+            maxDepthNodes.clear()
+            maxDepthNodes.addAll(newNodes)
+        }
+        
+        return maxDepthNodes.last()
     }
-    
-    return getLCA(root, 1)
-}
-
-// C++
-
-TreeNode* lcaDeepestLeaves(TreeNode* root) {
-    int depth = getDepth(root);
-    return getLCA(root, 1, depth);
-}
-
-int getDepth(TreeNode* root) {
-    if (root == nullptr) return 0;
-    return max(getDepth(root->left), getDepth(root->right)) + 1;
-}
-
-TreeNode* getLCA(TreeNode* root, int level, int depth) {
-    if (root == nullptr) return nullptr;
-    if (depth == level) return root;
-
-    auto left = getLCA(root->left, level + 1, depth);
-    auto right = getLCA(root->right, level + 1, depth);
-    
-    if (left != nullptr && right != nullptr) return root;
-    else if (left != nullptr) return left;
-    else return right;
 }
