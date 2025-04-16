4afbfe93f67a367cefeb08928e27dd55dbc947b1
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-04-16 10:14:23 +0530

    Update 2537.kt

--- 2501-3000/2537.kt
+++ 2501-3000/2537.kt
@@ -1,17 +1,20 @@
-fun countGood(nums: IntArray, k: Int): Long {
-    var l = 0
-    var count = 0L
-    var ans = 0L
-    val mp = mutableMapOf<Int, Int>()
-    for (r in nums.indices) {
-        count += mp.getOrDefault(nums[r], 0).toLong()
-        mp[nums[r]] = mp.getOrDefault(nums[r], 0) + 1
-        while (l < nums.size && count - (mp[nums[l]]!! - 1) >= k) {
-            mp[nums[l]] = mp[nums[l]]!! - 1
-            count -= mp[nums[l]]!!
-            l++
+class Solution {
+    fun countGood(A: IntArray, k: Int): Long {
+        var res = 0L
+        val count = HashMap<Int, Int>() // Hashmap to store the count of each element in the array
+        var i = 0
+        var j = 0
+        var k = k
+        while (j < A.size) {
+            k -= count.getOrDefault(A[j], 0) // subtract the count of current element from k
+            count[A[j]] = count.getOrDefault(A[j], 0) + 1 // increment the count of current element in the hashmap
+            while (k <= 0) {
+                count[A[i]] = count[A[i]]!! - 1 // decrement the count of the first element in the window
+                k += count[A[i++]]!! // add the count of the first element to k
+            }
+            res += i // add the index of the first element to the result
+            j++
         }
-        if (count >= k) ans += (l + 1)
+        return res
     }
-    return ans
 }
