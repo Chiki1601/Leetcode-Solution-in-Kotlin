742039540e91bc569f132b30e6045f6581f1f216
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-05-15 13:00:47 +0530

    Update 2900.kt

--- 2501-3000/2900.kt
+++ 2501-3000/2900.kt
@@ -1,19 +1,5 @@
 class Solution {
-  fun getLongestSubsequence(words: List<String>, groups: List<Int>): List<String> {
-    fun build(start: Int): List<String> {
-        val res = mutableListOf<String>()
-        var expect = start
-        for (i in words.indices) {
-            if (groups[i] == expect) {
-                res.add(words[i])
-                expect = expect xor 1
-            }
-        }
-        return res
+    fun getLongestSubsequence(w: Array<String>, g: IntArray) = buildList {
+        for (i in g.indices) if (i < 1 || g[i] != g[i - 1]) this += w[i]
     }
-
-    val res1 = build(0)
-    val res2 = build(1)
-    return if (res1.size >= res2.size) res1 else res2
-}  
 }
